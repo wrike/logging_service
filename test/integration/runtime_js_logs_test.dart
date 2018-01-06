38398e6f07969241abd5f60619509f5ac05b5e4f
Keksinautin <keksinautin@gmail.com>
2018-01-06 16:14:24 +0300

    Add js to dart logging

--- test/integration/runtime_js_logs_test.dart
+++ test/integration/runtime_js_logs_test.dart
@@ -7,17 +7,57 @@ import 'package:logging_service/src/configure_js_to_dart_logs_writer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('runProtected should', () {
-    test('protect from exceptions', () {
+  group('errors from js should', () {
+    test('be proxied to the Dart printer', () {
+      final loggingPrinterForBrowserMock = new LoggingPrinterForBrowserMock();
+
       print('use "print()" from Dart');
 
       final loggingService = new LoggingService();
       loggingService.start(rootLogLevel: log.Level.ALL);
-      loggingService.addLoggingPrinter(new LoggingPrinterForBrowser());
+      loggingService.addLoggingPrinter(loggingPrinterForBrowserMock);
 
       configureJsToDartLogsWriter(loggingService);
 
       html.window.console.log('use "window.console.log" from Dart');
+
+      final errorLogFromJs = loggingPrinterForBrowserMock.mf_captured[0];
+      expect(errorLogFromJs.message, 'Exception note');
+      expect(errorLogFromJs.error.toString(), 'ReferenceError: _nonexistentMethod is not defined');
+      expect(errorLogFromJs.level, log.Level.SHOUT); //TODO: change to Severe
+      expect(errorLogFromJs.loggerName, JsToDartLogsLoggerName);
+
+      final infoLogFromJs1 = loggingPrinterForBrowserMock.mf_captured[1];
+      expect(infoLogFromJs1.message, '{}');
+      expect(infoLogFromJs1.level, log.Level.WARNING);
+      expect(infoLogFromJs1.loggerName, JsToDartLogsLoggerName);
+
+      final infoLogFromJs2 = loggingPrinterForBrowserMock.mf_captured[2];
+      expect(infoLogFromJs2.message, '{"error":"some error-like object"}');
+      expect(infoLogFromJs2.level, log.Level.WARNING);
+      expect(infoLogFromJs2.loggerName, JsToDartLogsLoggerName);
+
+      final printFromDart = loggingPrinterForBrowserMock.mf_captured[4];
+      expect(printFromDart.message, 'use "print()" from Dart');
+      expect(printFromDart.level, log.Level.INFO);
+      expect(printFromDart.loggerName, JsToDartLogsLoggerName);
+
+      final consoleLogFromDart = loggingPrinterForBrowserMock.mf_captured[5];
+      expect(consoleLogFromDart.message, 'use "window.console.log" from Dart');
+      expect(consoleLogFromDart.level, log.Level.INFO);
+      expect(consoleLogFromDart.loggerName, JsToDartLogsLoggerName);
     });
   });
 }
+
+class LoggingPrinterForBrowserMock implements LoggingPrinterForBrowser {
+  // The mockito does not work correct with callable classes!
+  // ignore: non_constant_identifier_names
+  List<log.LogRecord> mf_captured = [];
+
+  @override
+  void call(log.LogRecord rec) {
+    mf_captured.add(rec);
+  }
+}
+
--- test/integration/runtime_js_logs_test_run.dart
+++ test/integration/runtime_js_logs_test_run.dart
@@ -1,6 +1,5 @@
 import 'runtime_js_logs_test.dart' as test_lib;
 
 void main() {
-  print('RUN');
   test_lib.main();
 }
