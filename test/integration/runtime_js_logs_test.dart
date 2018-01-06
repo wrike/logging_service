d595f06c3782e28d4f1713fea4526212f5729068
Keksinautin <keksinautin@gmail.com>
2018-01-06 15:00:01 +0300

    Add js to dart logging

--- test/integration/runtime_js_logs_test.dart
+++ test/integration/runtime_js_logs_test.dart
@@ -4,15 +4,20 @@ import 'package:logging/logging.dart' as log;
 import 'package:logging_service/logging_printer_for_browser.dart';
 import 'package:logging_service/logging_service.dart';
 import 'package:logging_service/src/configure_js_to_dart_logs_writer.dart';
+import 'package:test/test.dart';
 
 void main() {
-  print('use "print()" from Dart');
+  group('runProtected should', () {
+    test('protect from exceptions', () {
+      print('use "print()" from Dart');
 
-  final loggingService = new LoggingService();
-  loggingService.start(rootLogLevel: log.Level.ALL);
-  loggingService.addLoggingPrinter(new LoggingPrinterForBrowser());
+      final loggingService = new LoggingService();
+      loggingService.start(rootLogLevel: log.Level.ALL);
+      loggingService.addLoggingPrinter(new LoggingPrinterForBrowser());
 
-  configureJsToDartLogsWriter(loggingService);
+      configureJsToDartLogsWriter(loggingService);
 
-  html.window.console.log('use "window.console.log" from Dart');
+      html.window.console.log('use "window.console.log" from Dart');
+    });
+  });
 }
