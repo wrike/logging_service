b5642f4bf626a643824725a28ff4f98e065182ff
Keksinautin <keksinautin@gmail.com>
2018-01-09 20:24:29 +0300

    Add js to dart logging

--- lib/sentry_pre_save_for_browser.dart
+++ lib/sentry_pre_save_for_browser.dart
@@ -1,15 +1,20 @@
 import 'dart:html' as html;
 
+import 'package:logging_service/src/js_console_proxy.dart';
 import 'package:sentry_client/api_data/sentry_packet.dart';
 import 'package:sentry_client/api_data/sentry_request.dart';
 
 class SentryPreSaveForBrowser {
+  final JsConsoleProxy _consoleProxy;
+
+  SentryPreSaveForBrowser({JsConsoleProxy consoleProxy}) : _consoleProxy = consoleProxy ?? new JsConsoleProxy();
+
   void call(SentryPacket packet) {
     packet.culprit = Uri.base.toString();
     packet.serverName = Uri.base.host;
     packet.request = new SentryRequest(
         headers: {SentryRequest.userAgentHeaderKey: html.window.navigator.userAgent}, url: Uri.base.toString());
 
-    html.window.console.error('The sentry accident id: ${packet.eventId}');
+    _consoleProxy.error('The sentry accident id: ${packet.eventId}');
   }
 }
