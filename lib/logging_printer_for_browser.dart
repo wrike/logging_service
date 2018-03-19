a800b765c96f781d50aa0e70aee4a2a6bc4494b2
Keksinautin <keksinautin@gmail.com>
2018-03-19 19:06:54 +0300

    improve error experience

--- lib/logging_printer_for_browser.dart
+++ lib/logging_printer_for_browser.dart
@@ -11,45 +11,56 @@ class LoggingPrinterForBrowser {
         _consoleProxy = consoleProxy ?? new JsConsoleProxy();
 
   void call(log.LogRecord rec) {
-    var msg = '[${rec.time.toIso8601String()}] ${rec.loggerName}: ${rec.message}';
+    print('### Log: ${rec.sequenceNumber}');
+    print('### rec.level: ${rec.level}');
+    print('### rec.loggerName: ${rec.loggerName}');
+    print('### rec.time: ${rec.time}');
+    print('### rec.message: ${rec.message}');
+    print('### rec.error: ${rec.error}');
+    print('### rec.error.runtimeType: ${rec.error.runtimeType}');
+    print('### rec.stackTrace: ${rec.stackTrace}');
+    print('### rec.stackTrace.runtimeType: ${rec.stackTrace.runtimeType}');
 
-    if (rec.error != null && rec.error.toString() != rec.message) {
-      msg += '\n' + rec.error.toString();
-    }
 
-    if (rec.level == log.Level.SEVERE) {
-      _consoleProxy.error(msg);
-    } else {
-      _consoleProxy.log(msg);
-    }
-
-    if (rec.stackTrace != null) {
-      String trace;
-
-      if (_shouldTerseErrorWhenPrint) {
-        if (rec.stackTrace is Trace) {
-          trace = (rec.stackTrace as Trace).terse.toString();
-        } else if (rec.stackTrace is Chain) {
-          trace = (rec.stackTrace as Chain).terse.toString();
-        } else {
-          trace = new Trace.from(rec.stackTrace).terse.toString();
-        }
-      } else if (rec.stackTrace is Chain) {
-        _consoleProxy.group('The chained stack trace: ');
-        for (final trace in (rec.stackTrace as Chain).traces) {
-          _consoleProxy.log(_correctFormat(trace.original.toString()));
-        }
-        _consoleProxy.groupEnd();
-      } else if (rec.stackTrace is Trace) {
-        trace = _correctFormat((rec.stackTrace as Trace).original.toString());
-      } else {
-        trace = _correctFormat(rec.stackTrace.toString());
-      }
-
-      if (trace != null) {
-        _consoleProxy.log(trace);
-      }
-    }
+//    var msg = '[${rec.time.toIso8601String()}] ${rec.loggerName}: ${rec.message}';
+//
+//    if (rec.error != null && rec.error.toString() != rec.message) {
+//      msg += '\n' + rec.error.toString();
+//    }
+//
+//    if (rec.level == log.Level.SEVERE) {
+//      _consoleProxy.error(msg);
+//    } else {
+//      _consoleProxy.log(msg);
+//    }
+//
+//    if (rec.stackTrace != null) {
+//      String trace;
+//
+//      if (_shouldTerseErrorWhenPrint) {
+//        if (rec.stackTrace is Trace) {
+//          trace = (rec.stackTrace as Trace).terse.toString();
+//        } else if (rec.stackTrace is Chain) {
+//          trace = (rec.stackTrace as Chain).terse.toString();
+//        } else {
+//          trace = new Trace.from(rec.stackTrace).terse.toString();
+//        }
+//      } else if (rec.stackTrace is Chain) {
+//        _consoleProxy.group('The chained stack trace: ');
+//        for (final trace in (rec.stackTrace as Chain).traces) {
+//          _consoleProxy.log(_correctFormat(trace.original.toString()));
+//        }
+//        _consoleProxy.groupEnd();
+//      } else if (rec.stackTrace is Trace) {
+//        trace = _correctFormat((rec.stackTrace as Trace).original.toString());
+//      } else {
+//        trace = _correctFormat(rec.stackTrace.toString());
+//      }
+//
+//      if (trace != null) {
+//        _consoleProxy.log(trace);
+//      }
+//    }
   }
 
   String _correctFormat(String trace) {
