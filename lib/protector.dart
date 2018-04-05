222c2290a5be3bb8cffee901429e44317d903cb9
Keksinautin <keksinautin@gmail.com>
2018-04-05 14:45:13 +0300

    clear the protector buffer

--- lib/protector.dart
+++ lib/protector.dart
@@ -1,6 +1,11 @@
-
 class RepeatProtector {
+  static const int maxBufferSize = 1000;
   static final Set<dynamic> _errors = new Set<dynamic>();
 
-  static bool shouldBeHandled(dynamic error) => _errors.add(error);
+  static bool shouldBeHandled(dynamic error) {
+    if (_errors.length > maxBufferSize) {
+      _errors.clear();
+    }
+    return _errors.add(error);
+  }
 }
