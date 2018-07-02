83ea5b2d18f169389d351c704ff1dac2c9e15e62
Ivan Evsikov <keksinautin@gmail.com>
2018-07-02 06:40:09 +0300

    Dart 1 & 2

--- test/coverage.dart
+++
@@ -1,9 +0,0 @@
-import 'infinite_loop_protector_test.dart' as t1;
-import 'logging_saver_for_sentry_test.dart' as t2;
-import 'integration/configure_logging_for_browser_test.dart' as t3;
-
-void main() {
-  t1.main();
-  t2.main();
-  t3.main();
-}
